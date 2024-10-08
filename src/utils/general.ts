747bc0c38d373117912c587556095950c8691fc2
Herczeg Marcell <136583369+HMarcell94@users.noreply.github.com>
2024-10-08 20:29:29 +0200

    Delete src directory

--- src/utils/general.ts
+++
@@ -1,9 +0,0 @@
-export async function loadDictionary(name: string): Promise<Record<string, any>> {
-    const data = await import(`@/dictionary/${name}.json`, {
-        assert: {
-            type: "json"
-        }
-    });
-
-    return data.default;
-}
