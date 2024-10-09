741e3bec6f067e56f107d2e035190f678723721b
Herczeg Marcell <136583369+HMarcell94@users.noreply.github.com>
2024-10-09 21:19:32 +0200

    Add files via upload

---
+++ VizsgaCommon/SQL.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizsgaCommon
+{
+    public class SQL : DbContext
+    {
+        public static string ConnectionString { get; set; }
+
+        public SQL() : base(new DbContextOptionsBuilder().UseSqlServer(ConnectionString).Options)   { }
+
+ //        MySQL adatbázishoz (Pomelo.EntityframeworkCore.Mysql)
+ //
+ //       public SQL() : base(new DbContextOptionsBuilder().UseMySql(ConnectionString,ServerVersion.AutoDetect(ConnectionString)).Options) {}
+
+        public DbSet<LogEntry> Logs { get; set; }
+
+
+    }
+}
